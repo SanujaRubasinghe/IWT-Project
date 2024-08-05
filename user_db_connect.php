557d138d273f3011f1f0f9be0b9fed5195a69343
SanujaRubasinghe <121211054+SanujaRubasinghe@users.noreply.github.com>
2024-08-05 15:56:38 +0530

    Update user_db_connect.php

--- user_db_connect.php
+++ user_db_connect.php
@@ -2,9 +2,10 @@
     $servername = "dbshop.mysql.database.azure.com";
     $username = "sanuja";
     $password = "$AnuJA@2003";
-    $dbname = "userdata";
+    $dbname = "shop";
+    $tablename = "userdata";
 
-    $conn = mysqli_connect($servername, $username, $password, $dbname);
+    $conn = mysqli_connect($servername, $username, $password, $dbname, $tablename);
 
     if (!$conn) {
         die("Connection Error: ".mysqli_connect_error($conn));
